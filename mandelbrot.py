5672037748e8a2cc1426c617b3812c5977f6d66f
Maicon-Moreira <maiconmoreirapira@gmail.com>
2022-08-20 16:28:18 -0300

    feat: improve rendering

--- mandelbrot.py
+++ mandelbrot.py
@@ -5,10 +5,7 @@ from tqdm import tqdm
 import time
 
 
-NJIT_MODE = True
-
-
-@nb.jit(nopython=NJIT_MODE)
+@nb.njit()
 def mandelbrot_iterations(real, imag, max_iterations):
     """Determine the number of iterations for a complex number to escape the Mandelbrot set."""
     c = complex(real, imag)
@@ -20,13 +17,13 @@ def mandelbrot_iterations(real, imag, max_iterations):
     return i
 
 
-@nb.jit(nopython=NJIT_MODE)
+@nb.njit(parallel=True)
 def generate_mandelbrot_iterations_array(x1, y1, x2, y2, width, height, max_iterations):
     """Generate the Mandelbrot iterations array."""
     x_step = (x2 - x1) / width
     y_step = (y2 - y1) / height
     iterations = np.zeros((width, height), dtype=np.uint16)
-    for x in range(width):
+    for x in nb.prange(width):
         for y in range(height):
             iterations[x, y] = mandelbrot_iterations(
                 x1 + x * x_step, y1 + y * y_step, max_iterations
@@ -34,11 +31,11 @@ def generate_mandelbrot_iterations_array(x1, y1, x2, y2, width, height, max_iter
     return iterations
 
 
-@nb.jit(nopython=NJIT_MODE)
+@nb.njit(parallel=True)
 def map_mandelbrot_iterations_to_grayscale(iterations, max_iterations):
     """Map the Mandelbrot iterations array to a grayscale image."""
     image = np.zeros((iterations.shape[0], iterations.shape[1]), dtype=np.uint8)
-    for x in range(iterations.shape[0]):
+    for x in nb.prange(iterations.shape[0]):
         for y in range(iterations.shape[1]):
             if iterations[x, y] == max_iterations:
                 image[x, y] = 0
@@ -58,10 +55,12 @@ def render_mandelbrot(
     width, height = pixels_width / scaled_zoom, pixels_height / scaled_zoom
     x1, x2 = center_x - width / 2, center_x + width / 2
     y1, y2 = center_y - height / 2, center_y + height / 2
-    print(f"Rendering Mandelbrot set with center ({center_x}, {center_y}) and zoom {zoom}")
+    print(
+        f"Rendering Mandelbrot set with center ({center_x}, {center_y}) and zoom {zoom}"
+    )
     print(f"x1 = {x1}, x2 = {x2}, y1 = {y1}, y2 = {y2}")
 
-    print('-'*32)
+    print("-" * 32)
     print("Generating Mandelbrot iterations array...", end="")
     start = time.time()
     iterations = generate_mandelbrot_iterations_array(
@@ -69,7 +68,7 @@ def render_mandelbrot(
     )
     print(f" done in {time.time() - start:.2f} seconds")
 
-    print('Mapping Mandelbrot iterations to grayscale...', end="")
+    print("Mapping Mandelbrot iterations to grayscale...", end="")
     start = time.time()
     image = Image.fromarray(
         map_mandelbrot_iterations_to_grayscale(iterations, max_iterations).T
@@ -83,7 +82,12 @@ def render_mandelbrot(
 
 
 def main():
-    render_mandelbrot(-0.5, 0, 1, 1920, 1080, 100, "mandelbrot.png")
+    # 1920 x 1080
+    # 2560 x 1440
+    # 3840 x 2160
+    # 7680 x 4320
+
+    render_mandelbrot(-0.74, -0.15, 75, 3840, 2160, 500, "mandelbrot.png")
 
 
 if __name__ == "__main__":
